4ad101641659536e82b58b2541fdcc7f042b01ec
Darth Vader <danieltarancon@gmail.com>
2023-06-22 18:38:40 +0200

    22 1838

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,39 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game {
+
+    constructor(missed = 0, phrases, activePhrase=null) {
+
+        this.missed = missed;
+        this.phrases = phrases;
+        this.activePhrase = activePhrase;
+
+
+    }
+
+
+    getRandomPhrase(){
+
+
+
+
+    }
+
+    startGame(){
+
+        // Hides the start screen overlay
+
+        this.activePhrase = getRandomPhrase();
+
+        addPhraseToDisplay(this.activePhrase);
+
+
+
+    }
+
+
+
+
+}
