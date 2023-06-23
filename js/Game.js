c2942d30a7407c2753af507c7f1fa17d1889721a
Darth Vader <danieltarancon@gmail.com>
2023-06-23 19:49:04 +0200

    23 1949

--- js/Game.js
+++ js/Game.js
@@ -5,147 +5,78 @@
 
 const keyButtons = document.querySelectorAll('#qwerty .keyrow .key');
 let letterButton;
-const overlaySreen = document.getElementById('overlay');
+const overlayScreen = document.getElementById('overlay');
 const gameOverMessage = document.getElementById('game-over-message');
 
 class Game {
+  constructor(missed = 0, phrases, activePhrase = null) {
+    this.missed = missed;
+    this.phrases = phrases;
+    this.activePhrase = activePhrase;
+  }
 
-    constructor(missed = 0, phrases, activePhrase=null) {
+  getRandomPhrase() {
+    let index = Math.floor(Math.random() * this.phrases.length);
+    return this.phrases[index];
+  }
 
-        this.missed = missed;
-        this.phrases = phrases;
-        this.activePhrase = activePhrase;
+  handleInteraction(letter) {
+    keyButtons.forEach(button => {
+      button.addEventListener('click', function(event) {
+        const clickedButton = event.target;
+        keyButtons.forEach(btn => {
+          if (btn === clickedButton) {
+            btn.disabled = true;
+            const isMatch = this.activePhrase.includes(letter);
+            if (!isMatch) {
+              btn.classList.add('wrong');
+              this.removeLife();
+            } else {
+              btn.classList.add('chosen');
+              this.showMatchedLetter(this.activePhrase);
+              this.checkForWin();
+              // If the player has won the game, call the gameOver() method.
+            }
+          }
+        });
+      });
+    });
+  }
 
-
+  removeLife() {
+    const heartImages = document.querySelectorAll('#scoreboard .tries img');
+    const missed = document.querySelector('#scoreboard .tries:last-child');
+    if (heartImages.length > 0) {
+      heartImages[heartImages.length - 1].src = 'images/lostHeart.png';
+      missed.classList.remove('tries');
+      missed.classList.add('lost');
     }
-
-
-    getRandomPhrase(){
-
-        let index = Math.floor(Math.random()*this.phrases.length);
-
-        return this.phrases[index];
-
-
+    this.missed++;
+    if (this.missed === 5) {
+      this.gameOver();
     }
+  }
 
-    handleInteraction(letter) {
+  checkForWin() {
+    // Checks if the phrase has been guessed. So if it is revealed.
+  }
 
-        keyButtons.forEach(button=> {
-
-            button.addEventListener('click', function(event){
-
-                const clickedButton = event.target;
-
-                keyButtons.forEach(btn => {
-
-                    if (btn === clickedButton) {
-
-                        btn.disabled = true;
-
-                        const isMatch = this.activePhrase.includes(letter);
-
-                        if(!isMatch) {
-
-                            btn.class.List.add('wrong');
-                            this.removeLife();
-
-
-                        } else {
-
-                            btn.class.List.add('chosen');
-                            this.showMatchedLetter(this.activePhrase);
-                            this.checkForWin()
-                            // If the player have won the game call the gameOver() method.
-
-
-                        }
-
-
-
-                    }
-
-                })
-
-            })
-
-        })
-
-
-
-
+  gameOver() {
+    overlayScreen.style.display = 'block';
+    if (this.missed === 5) {
+      gameOverMessage.textContent = 'You lost! Try again.';
+      overlayScreen.classList.remove('start');
+      overlayScreen.classList.add('lose');
+    } else {
+      gameOverMessage.textContent = 'Congratulations! You won!';
+      overlayScreen.classList.remove('start');
+      overlayScreen.classList.add('win');
     }
+  }
 
-
-    removeLife(){
-
-        const heartImages = document.querySelectorAll('#scoreboard .tries img');
-        const missed = document.querySelector('#scoreboard .tries:last-child');
-
-        if (heartImages.length > 0) {
-
-            heartImages[heartImages.length - 1].src = 'images/lostHeart.png';
-            missed.classList.remove('tries');
-            missed.classList.add('lost');
-
-        }
-
-        this.missed++;
-
-        if (this.missed === 5) {
-
-            this.gameOver()
-
-        }
-
-
-        }
-
-
-    }
-
-    checkForWin(){
-
-    // Checks if the phrase has been guessed. So if it is reveal.
-
-    }
-
-
-    gameOver(){
-
-        overlaySreen.style.display = 'block';
-
-        if (this.missed === 5) {
-
-            gameOverMessage.textContent = 'You lost! Try again.';
-            overlayScreen.classList.remove('start');
-            overlayScreen.classList.add('lose');
-
-
-        } else {
-
-                gameOverMessage.textContent = 'Congratulations! You won! ';
-                overlayScreen.classList.remove('start');
-                overlayScreen.classList.add('win');
-
-        }
-
-    }
-
-
-
-    startGame() {
-
+  startGame() {
     overlayScreen.style.display = 'none';
-
-    this.activePhrase = getRandomPhrase();
-
+    this.activePhrase = this.getRandomPhrase();
     addPhraseToDisplay(this.activePhrase);
-
-
- }
-
-
-
-
+  }
 }
